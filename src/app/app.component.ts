0832c4cab2493063498c406b6fd017bd1eef8ffc
hu <husseinghazala39@gmail.com>
2024-10-19 19:58:00 +0300

    depi harvest

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { RouterOutlet, Router, Event, NavigationEnd } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+})
+export class AppComponent {
+    title = 'Harvest - Hussein Ghazala';
+
+    constructor(
+        private router: Router,
+        private viewportScroller: ViewportScroller
+    ) {
+        this.router.events.subscribe((event: Event) => {
+            if (event instanceof NavigationEnd) {
+                this.viewportScroller.scrollToPosition([0, 0]);
+            }
+        });
+    }
+}
